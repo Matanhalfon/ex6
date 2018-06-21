c30009e304068a3e3a77898a92e7844795b8e19b
yahel <yahel.yedidya@mail.huji.ac.il>
2018-06-21 17:17:44 +0300

    end Sjava and Parser

--- src/oop/ex6/main/Parser.java
+++ src/oop/ex6/main/Parser.java
@@ -5,46 +5,73 @@ import java.io.*;
 import java.util.*;
 import java.util.regex.*;
 
+/**
+ * A class that parse the file to lines array
+ */
 public class Parser {
+    /*A constant that represent the message in case of not found file*/
+    private static final String NO_FILE_ERR_MSG = "ERROR: not found file";
+
+    /*A constant that represent the message in case of problem in the file reading*/
+    private static final String CANT_READ_ERR_MSG = "ERROR: can not read the file";
+    /*A buffer reader*/
     private BufferedReader Reader;
-    private ArrayList<String> GOODLINES;
-    //REGEX
-    String BadLine = "^\\/{2,}.*|\\s*";
-    Pattern Nolines = Pattern.compile(BadLine);
+
+    /*Array List that holding all the lines*/
+    private ArrayList<String> GoodLines;
+
+    /*A constant that represent the regex of bad line*/
+    private final String BADLINE = "^\\/{2,}.*|\\s*";
+
+    /*A constant that represent the pattern of the regex of bad line*/
+    private final Pattern NOLINE = Pattern.compile(BADLINE);
 
 
+    /**
+     * Initialize a parser that convert the file to ArrayList that hold all the good
+     * lines.
+     * @param jfile the Sjavac file we needed to convert
+     * @throws IOEx in case of IO exception
+     */
     public Parser(File jfile) throws IOEx {
         try {
-
             ArrayList<String> ParsedFiles = new ArrayList<String>();
             this.Reader = new BufferedReader(new FileReader(jfile));
-            this.GOODLINES = new ArrayList<String>();
+            this.GoodLines = new ArrayList<String>();
             ParsedFiles(jfile);
         } catch (FileNotFoundException e) {
-            throw new IOEx("ERROR: not found file");
+            throw new IOEx(NO_FILE_ERR_MSG);
         }
     }
 
+    /**
+     * A method that gets file and convert it to ArrayList without the lines it need to ignore
+     * @param jfile the file we want to convert
+     * @throws IOEx in case of IO exception
+     */
     private void ParsedFiles(File jfile) throws IOEx {
         try {
             String curLine = this.Reader.readLine();
             while (curLine != null) {
-                Matcher match = Nolines.matcher(curLine);
+                Matcher match = NOLINE.matcher(curLine);
                 if (!match.matches()) {
-                    GOODLINES.add(curLine);
+                    GoodLines.add(curLine);
                 }
                 curLine = Reader.readLine();
 
             }
         } catch (IOException e) {
-            throw new IOEx("ERROR: can not read the file");
+            throw new IOEx(CANT_READ_ERR_MSG);
 
         }
     }
 
-
-        public ArrayList<String> getGOODLINES () {
-            return GOODLINES;
+    /**
+     * retuens the ArrayList of the good lines
+     * @return the list of the good lines
+     */
+    public ArrayList<String> getGoodLines() {
+            return GoodLines;
         }
     }
 
--- src/oop/ex6/main/Sjavac.java
+++ src/oop/ex6/main/Sjavac.java
@@ -1,34 +1,59 @@
 package oop.ex6.main;
+
+
 import java.io.*;
-
-import com.sun.tools.javac.Main;
-
-import java.io.IOException;
-import java.nio.file.InvalidPathException;
-import java.text.ParseException;
 import java.util.ArrayList;
 
-
+/**
+ * This class is the main class. It implements all the checks for the Sjavac
+ * file by using other classes.
+ */
 public class Sjavac {
 
+    /*A constant of the wrong number of arguments message*/
+    private static final String ARGS_ERR_MSG = "ERROR: to many arguments";
+
+    /*A constant that represent the path to the file in the commend line*/
+    private static final int PATH = 0;
+
+    /*A constant that represent the printed line in compilation error case*/
+    private static final int COMPEX = 1;
+
+    /*A constant that represent the good length of the arguments line*/
+    private static final int GOODLENGTH = 1;
+
+    /*A constant that represent the printed line in IO error case*/
+    private static final int IOEX = 2;
+
+    /*A constant that represent the printed line in case of all checks pass*/
+    private static final int ALLGOOD = 0;
+
+    /**
+     * The main method. Gets the file path as argument, convert it to Array of lines, and
+     * implements all the checks that requires.
+     * @param args the path to the Sjavac file
+     */
     public static void main(String[] args)  {
         try {
-            File javaFile=new File(args[0]);
+            if (args.length!= GOODLENGTH) {
+                throw new IOEx(ARGS_ERR_MSG);
+            }
+            File javaFile=new File(args[PATH]);
             Parser parser = new Parser(javaFile);
-            ArrayList<String> SjavaFiles = parser.getGOODLINES();
+            ArrayList<String> SjavaFiles = parser.getGoodLines();
             MainBlock compaler = new MainBlock(SjavaFiles);
             compaler.readLines();
 
             } catch(CompEx compEx){
-                System.out.println(1);
+                System.out.println(COMPEX);
                 return;
             }
             catch (IOEx e){
-                System.out.println(2);
+                System.out.println(IOEX);
                 System.err.println(e.getMessage());
                 return;
             }
-        System.out.println(0);
+        System.out.println(ALLGOOD);
         }
 
     }
